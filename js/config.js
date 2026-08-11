9e4857ab3bdbefa0e06e8bffb714ccf8442c39d8
Blacktophero <vhunt515@gmail.com>
2026-08-11 16:13:28 -0400

    Modify userPoolClientId in js/config.js
    Updated userPoolClientId in configuration.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_H0YeMtYh5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ejafdvfedtrsk33tv2vcs57l5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1l2s6o84s2715u7l5l56qpnj13', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
